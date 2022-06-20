ec836753e26c77ddeae5009a813644e0a3d48d21
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-06-20 11:32:18 +0200

    route modification

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as  Router, Routes, Route } from "react-router-dom";
+import './css/style.css';
+import CreateEmployee from './pages/CreateEmployee';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+          <Route path="/" element={<CreateEmployee />}/>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
