cdf2771a8f13a52b037ad22909bedd1547482ad2
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-06-21 16:10:55 +0200

    add employee table name columns

---
+++ src/data/employeeColumnsTable.js
@@ -0,0 +1,87 @@
+const employeeColumnsTable = [
+    {
+        Header: 'first Name',
+        columns: [
+            {
+                Header: 'first Name',
+                accessor: 'firstName',
+            },
+        ],
+    },
+    {
+        Header: 'last Name',
+        columns: [
+            {
+                Header: 'last Name',
+                accessor: 'lastName',
+            },
+        ],
+    },
+    {
+        Header: 'start Date',
+        columns: [
+            {
+                Header: 'start Date',
+                accessor: 'startDate',
+            },
+        ],
+    },
+    {
+        Header: 'department',
+        columns: [
+            {
+                Header: 'department',
+                accessor: 'department',
+            },
+        ],
+    },
+    {
+        Header: 'Date of birth',
+        columns: [
+            {
+                Header: 'date od birth',
+                accessor: 'dateOfBirth',
+            },
+        ],
+    },
+    {
+        Header: 'Street',
+        columns: [
+            {
+                Header: 'street',
+                accessor: 'street',
+            },
+        ],
+    },
+    {
+        Header: 'City',
+        columns: [
+            {
+                Header: 'city',
+                accessor: 'city',
+            },
+        ],
+    },
+    {
+        Header: 'State',
+        columns: [
+            {
+                Header: 'state',
+                accessor: 'state',
+            },
+        ],
+    },
+    {
+        Header: 'zip Code',
+        columns: [
+            {
+                Header: 'zip code',
+                accessor: 'zipCode',
+            },
+        ],
+    },
+]
+   
+
+
+    export default employeeColumnsTable;
